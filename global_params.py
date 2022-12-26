4fe62b503c552ec47a6f79db5839097a71a3e840
Galin Georgiev <galinngeorgiev@users.noreply.github.com>
2022-12-26 12:31:39 -0800

    Update global_params.py

--- global_params.py
+++ global_params.py
@@ -12,7 +12,7 @@ NUM_LP, MAX_TIMES_INCREASE_NUM_LP RUNTIME_SEC = 3, 1
 NUM_INVESTED_TOKEN1_LP = 10
 #N.B. UNWIND_DIST_TO_BOUND_PER is more robust than UNWIND_ASSET_RATIO_PER because the distance does not depend on price_LP (which could be very different than API pool prices)
 ##UNWIND_ASSET_RATIO_PER = 80
-LP_DISTANCE_TO_BOUND_PER,  LP_BOUND_DISTANCE_TO_CURRENT_PER = [0., 0.], 0.5 #if 0., price range is the minimum # ticks > 0, determined by the pool (fee); 2-nd entry is for begining-to-end of quiet hours, 1-st entry otherwise!
+LP_DISTANCE_TO_BOUND_PER,  LP_BOUND_DISTANCE_TO_CURRENT_PER = [0., 0.], 0.5 #if 0., price range is the minimum # ticks > 0, determined by the pool (fee); 2-nd entry in the list is for begining-to-end of quiet hours, 1-st entry in list otherwise!
 #N.B. Valid for LP_position_to_init_price = 1; if LP_position_to_init_price = -1, the pair has to be reversed when forming a tuple !
 LP_UNWIND_DISTANCE_TO_LOWER_BOUND_DECREASE_PER, LP_UNWIND_DIST_TO_UPPER_BOUND_PER = 0.2, 0.
 #INCREASE_LIQUIDITY = False #N.B. This flag is for increaseLiquidity() without mint(); not working properly: after execution, has to goes straight to mint(), without decreaseLiquidity() but not otherwise!
